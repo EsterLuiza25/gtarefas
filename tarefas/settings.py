ab44f7c85a5e3041b01bc3cfa6834b1fcf9cb5e5
luiza <luiza.campossouza.25@gmail.com>
2026-03-16 21:17:50 -0300

    Fix: configurando STATIC_ROOT e WhiteNoise para deploy

--- tarefas/settings.py
+++ tarefas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#pc^!8#$a6m)sze8wg1smr8grkzz1=$sn%!#(6ddz)3^6q9w#c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,5 +121,10 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
